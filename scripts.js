a2a6d2f43d9768fdd12fb8fb134ceccd20daa5d4
Saharsh Yeruva <hello@saharsh.tech>
2020-07-24 14:26:14 -0400

    Added code from workshop, but not working

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function onMouseMove(event) {
+var path = new Path.Circle({
+  center: event.middlePoint,
+  radius: 10
+})
+
+path.fillColor = {
+  hue: 0,
+  saturation: 1,
+  brightness: 1
+}
+}
+
